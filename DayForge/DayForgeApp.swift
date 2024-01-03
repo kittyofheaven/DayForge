7346a51fd212e70beac1f99d881033a7fc00f55d
citylighxts <hanazizahn@gmail.com>
2024-01-03 13:30:12 +0700

    Initial Commit

---
+++ DayForge/DayForgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayForgeApp.swift
+//  DayForge
+//
+//  Created by Hana Azizah on 03/01/24.
+//
+
+import SwiftUI
+
+@main
+struct DayForgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
